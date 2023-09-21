9e5c29fedc393d36662c619f8551b03efbd2b0fa
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-21 20:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
-//Add your code here
-  
-};
+// //Add your code here
+	const rates=document.getElementsByClassName("price");
+
+	let price=0;
+	for(let i=0;i<rates.length;i++){
+		if(parseInt(rates[i].textContent)!=NaN){
+			price+=parseInt(rates[i].textContent)
+		}
+	}
+
+	let newRow=document.createElement("tr");
+	newRow.innerHTML=`<td></td>
+//  <td>${price}</td>`;
+
+	const table=document.getElementsByTagName("TABLE")[0];
+	table.appendChild(newRow);
+}
 
 getSumBtn.addEventListener("click", getSum);
-
